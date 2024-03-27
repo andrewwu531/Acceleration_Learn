0a04d97ff05e18e75ff2347b8fcc9429e192d5d4
unknown <andrewwu0531>
2024-03-27 00:29:16 +0000

    completed mission statement

--- src/components/TeamMissionStatement.jsx
+++ src/components/TeamMissionStatement.jsx
@@ -6,42 +6,28 @@
 
      <div className="flex flex-col items-center justify-center max-w-3xl mx-auto xl:max-w-4xl bg-gray-150" style={{ marginBottom: "17vh"}}>
 
-        <div className="flex justify-center text-2xl font-semibold text-center underline xl:text-3xl"  style={{ marginTop: "12vh", marginBottom: "5vh"}}>
+        <div className="text-2xl font-semibold underline xl:text-3xl"  style={{ marginTop: "12vh", marginBottom: "4vh"}}>
             Mission Statement
         </div>
 
         <div className="text-xl font-normal">  
 
-            Hi there, 
-            <br /><br />
-            I’m Andrew. I am a Hong Kong UK immigrant who came to the UK at the age of 13 in 2009. 
+            <br />
+            Acceleration Learn was found with the ambition to help you become as successful as possible
+            in bodybuilding and acquiring the English language.
 
             <br /><br />
-            My life in the UK has been too good to be true and also full of challenges and tears. 
-            I did not speak the right English pronunciation 
-            nor was I able to communicate fluently with other people when I first started living in the UK for many years.
+            We are here to make your life count and we develop these programmes with every resources we can get.
             
             <br /><br />
-            The mission of these programmes is to share with you the unique skill sets that
-            I have acquired through my university degree and life experience.
-            I don’t want you to struggle in life just like I did.
+            We urge you to read our free Excel materials and videos -
+            we will prove to you that our courses are worth way more than their costs.
             
             <br /><br />
-            I share your present pain and suffering. 
-            I want you to live a life that is worth living instead of letting time pass by simply
-            because you are not equipped with the knowledge and skills 
-            that would enable you to achieve and live the greater version of life that you aspire and deserve.
+            I wish you will gain valuable skills with Acceleration Learn and achieve further than where you want to be.
             
-            <br /><br />
-            I developed these programmes with all of my heart. 
-            I treat these courses as the life-long goal that must be excelled 
-            and I want you to gain the same skill sets with me in a much shorter time frame.
-            
-            <br /><br />
-            I hope you will find my courses practical and of use and I also sincerely wish that you will achieve more than me.
-            
-            <br /><br />
-            Regards,<br /><br />
+            <br /><br /><br />
+         
             Andrew Wu<br />
             Acceleration Learn
         </div>
